5d42e02958d56643aadd590b35c9b58f64dd3037
Cielo <houng1004@gmail.com>
2023-08-23 23:12:01 +0800

    feat:新增api route、新增建立user的驗證

--- server.js
+++ server.js
@@ -3,10 +3,23 @@ const connectDB = require('./config/db');
 
 const app = express();
 
+const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
+const posts = require('./routes/api/posts');
+
 //Connect Database
 connectDB();
 
+//Init Middleware
+app.use(express.json({ extended: false}));
+
 app.get('/', (req,res)=> res.send('API Running'));
+
+//Define Routes
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+app.use('/api/posts', posts);
+
 
 const PORT = process.env.PORT || 5000;
 
